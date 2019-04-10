52cc508717b63170d1df715c28e23955e2af30cb
acerise <42525915+acerise85@users.noreply.github.com>
2019-04-09 21:27:56 -0700

    fixing heroku

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 module.exports = {
     article: require("./articles"),
-    savedArticle: require("./savedArticle")
+    // savedArticle: require("./savedArticle")
 };
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,6 +2,6 @@
 
 module.exports = function(app) {
     app.get('/scrape')
-    app.get('/savedArticle')
+    // app.get('/savedArticle')
 };
 
